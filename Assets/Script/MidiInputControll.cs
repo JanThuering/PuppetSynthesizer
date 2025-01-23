dd8eb769b1a342820e10060fa3045b7d57e45422
JanThuering <jp.thuering@bluewin.ch>
2025-01-23 17:35:39 +0100

    Same as before but without compilatione errors

--- Assets/Script/MidiInputControll.cs
+++ Assets/Script/MidiInputControll.cs
@@ -23,28 +23,13 @@ public class MidiInputControll : MonoBehaviour {
         var midiDevice = (MidiDevice)sender;
         //Debug.Log($"Received MIDI event {e.Event} from {midiDevice.Name}");
         Debug.Log(e.Event);
-        
+
         /*
         if (e.Event is NoteOnEvent noteOn) {
             Debug.Log($"Note {noteOn.NoteNumber} pressed with velocity {noteOn.Velocity}");
         }
         */
     }
-
-
-
-    /*
-    void OnEventReceived(object sender, MidiEventReceivedEventArgs e) {
-    var midiDevice = (MidiDevice)sender;
-
-    if (e.Event is NoteOnEvent noteOn) {
-        // Map MIDI velocity (0-127) to desired range and update CreateLine
-        float mappedValue = noteOn.Velocity / 127f;  // Normalize to 0-1
-        _csCreateLine._debugMidiValue = mappedValue;
-        Debug.Log($"Updated MIDI value to {mappedValue}");
-    }
-    }
-    */
 
     void OnDisable() {
         if (input != null) {
