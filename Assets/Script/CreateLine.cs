70a899b20555907f40a7820f15c991a700d7269e
JanThuering <jp.thuering@bluewin.ch>
2025-01-25 15:34:45 +0100

    New WaveSystem (MIDI Input currently not connected)

--- Assets/Script/CreateLine.cs
+++ Assets/Script/CreateLine.cs
@@ -8,7 +8,7 @@ public class CreateLine : MonoBehaviour
 {
     [Header ("DEBUGGING")]
     [SerializeField] private bool _debug = true;
-    public float _debugMidiValue = 0;
+    [SerializeField] private bool segmentedCurves = false;
 
     [Header ("EXTERNAL REFERENCES")]
     [SerializeField] private LineRenderer _lineRenderer;
@@ -16,39 +16,57 @@ public class CreateLine : MonoBehaviour
     [SerializeField] private GameObject _lineEnd;
 
     [Header ("ANIMATION VALUES")]
+  
     [SerializeField] private Vector3 _movement = new Vector3(0,1,0);
 
     [Range(-5, 5)]
     [SerializeField] private float _totalAmplitude = 1;
     [Range(1f, 10)]
     public float _frequency = 1;
-    [Range(0f, 500f)]
+    [Range(0f, 5)]
     public float _horizontalMovement = 10;
 
     [Header ("CURVES")]
+    [SerializeField] private AnimationCurve [] _curveArray;
+    [SerializeField] private float [] _amplitudeArray;
+    [SerializeField] private float [] _speedArray;
+
+
     [SerializeField] private AnimationCurve _curveA;
-    [Range(-10, 10)]
-    public float _amplitudeA = 1;
+    [Range(-5, 5)]
+    [SerializeField] float _amplitudeA = 1;
+    [Range(-5, 5)]
+    [SerializeField] float _speedA = 1;
 
     [SerializeField] private AnimationCurve _curveB;
-    [Range(-10, 10)]
-    public float _amplitudeB = 1;
+    [Range(-5, 5)]
+    [SerializeField] float _amplitudeB = 1;
+    [Range(-5, 5)]
+    [SerializeField] float _speedB = 1;
 
     [SerializeField] private AnimationCurve _curveC;
-    [Range(-10, 10)]
-    public float _amplitudeC = 1;
+    [Range(-5, 5)]
+    [SerializeField] float _amplitudeC = 1;
+    [Range(-5, 5)]
+    [SerializeField] float _speedC = 1;
 
     [SerializeField] private AnimationCurve _curveD;
-    [Range(-10, 10)]
-    public float _amplitudeD = 1;
+    [Range(-5, 5)]
+    [SerializeField] float _amplitudeD = 1;
+    [Range(-5, 5)]
+    [SerializeField] float _speedD = 1;
 
     [SerializeField] private AnimationCurve _curveE;
-    [Range(-10, 10)]
-    public float _amplitudeE = 1;
+    [Range(-5, 5)]
+    [SerializeField] float _amplitudeE = 1;
+    [Range(-5, 5)]
+    [SerializeField] float _speedE = 1;
 
     [SerializeField] private AnimationCurve _curveF;
-    [Range(-10, 10)]
-    public float _amplitudeF = 1;
+    [Range(-5, 5)]
+    [SerializeField] float _amplitudeF = 1;
+    [Range(-5, 5)]
+    [SerializeField] float _speedF = 1;
     
     [Header ("INTERFACE VALUES")]
     [SerializeField] private int _sliderAmmount = 3;
@@ -82,24 +100,65 @@ public class CreateLine : MonoBehaviour
     void Start()
     {
         CreatePoints();
+        
     }
 
     // Update is called once per frame
     void Update()
     { 
         FillLineRenderer(); //fill the line renderer with the points
+        if(segmentedCurves){    //old curvesystem
+            // Move the points in the defined axis, acording to the curves
+            ControlCurve(1, _curveA, "Curve A", _amplitudeA, _frequency);
+            ControlCurve(2, _curveB, "Curve B", _amplitudeB, _frequency);
+            ControlCurve(3, _curveC, "Curve C", _amplitudeC, _frequency);
+            ControlCurve(4, _curveD, "Curve D", _amplitudeD, _frequency);
+            ControlCurve(5, _curveE, "Curve E", _amplitudeE, _frequency);
+            ControlCurve(6, _curveF, "Curve F", _amplitudeF, _frequency);
 
-        //Move the points in the defined axis, acording to the curves
-        ControlCurve(1, _curveA, "Curve A", _amplitudeA, _frequency);
-        ControlCurve(2, _curveB, "Curve B", _amplitudeB, _frequency);
-        ControlCurve(3, _curveC, "Curve C", _amplitudeC, _frequency);
-        ControlCurve(4, _curveD, "Curve D", _amplitudeD, _frequency);
-        ControlCurve(5, _curveE, "Curve E", _amplitudeE, _frequency);
-        ControlCurve(6, _curveF, "Curve F", _amplitudeF, _frequency);
+        }else{  //new curvesystem
+            FillCurveArray(); //fill the curve array with the curves and amplitudes
+            ControlGlobalCurve(); //Move the points in the defined axis, acording to the curves
+        }
+
+
 
     }
 
+    private void FillCurveArray(){
+        //Fills the arrays with the curves and amplitudes
+        //easiere access to the values for the MIDI input
+        _curveArray = new AnimationCurve[7];
+        _amplitudeArray = new float[7];
+        _speedArray = new float[7];
 
+        _amplitudeArray [0] = _totalAmplitude;
+        _speedArray[0] = _horizontalMovement;
+
+        _curveArray[1] = _curveA;
+        _amplitudeArray[1] = _amplitudeA;
+        _speedArray[1] = _speedA;
+
+        _curveArray[2] = _curveB;
+        _amplitudeArray[2] = _amplitudeB;
+        _speedArray[2] = _speedB;
+
+        _curveArray[3] = _curveC;
+        _amplitudeArray[3] = _amplitudeC;
+        _speedArray[3] = _speedC;
+
+        _curveArray[4] = _curveD;
+        _amplitudeArray[4] = _amplitudeD;
+        _speedArray[4] = _speedD;
+
+        _curveArray[5] = _curveE;
+        _amplitudeArray[5] = _amplitudeE;
+        _speedArray[5] = _speedE;
+
+        _curveArray[6] = _curveF;  
+        _amplitudeArray[6] = _amplitudeF;
+        _speedArray[6] = _speedF;
+    }
 
     private void CreatePoints(){    
         //check if (points count + 2) is divisible by slider ammount
@@ -153,6 +212,11 @@ public class CreateLine : MonoBehaviour
         }
     }
 
+    public void InputToValues(int controlNumber, float controlValue){
+        AnimationCurve curve = _curveArray[controlNumber];
+        _amplitudeArray[controlNumber] = controlValue; 
+    }
+
     private void ControlCurve(int slider, AnimationCurve curve, string curveName, float amplitude, float frequency){
         //check if slider is in range
         if(slider > _sliderAmmount || slider < 1){
@@ -163,7 +227,7 @@ public class CreateLine : MonoBehaviour
         }
 
         //counts up so the wave moves up the index
-        float timeCounter = Time.time*_horizontalMovement;
+        float timeCounter = Time.time*_horizontalMovement*100;
 
         //calculate range of points
         int range = _pointsArray.Length / _sliderAmmount;
@@ -177,6 +241,7 @@ public class CreateLine : MonoBehaviour
             
             // Normalize evaluation value between 0 and 1 for current segment
             float normalizedT = ((float)(i - (start+(int)timeCounter)) / range) * frequency;
+
             // Use modulo to wrap value between 0-1
             normalizedT = normalizedT % 1.0f;
             
@@ -198,5 +263,35 @@ public class CreateLine : MonoBehaviour
     }
 
 
+    private void ControlGlobalCurve(){
+        // counts up so the wave moves up the index
+        float timeCounter = Time.time * _horizontalMovement;
+
+        // Move points in the defined axis, according to the curves
+        for (int i = 0; i < _pointsArray.Length; i++) {
+            int currentIndex = i % _pointsArray.Length;
+            float combinedCurveValue = 0;
+
+            //Combine all curves
+            for (int j = 1; j < _sliderAmmount + 1; j++) {  // Evaluate the curve based on the current index and frequency
+                float curveEvaluation = _curveArray[j].Evaluate((currentIndex / (float)_pointsArray.Length) * _frequency + timeCounter * _speedArray[j]);
+                combinedCurveValue += curveEvaluation * _amplitudeArray[j] * _totalAmplitude;
+            }
+
+            //Move in defined direction
+            _movement = _movement.normalized;
+            Vector3 right = _pointsArray[currentIndex].transform.right * combinedCurveValue * _movement.x;
+            Vector3 up = _pointsArray[currentIndex].transform.up * combinedCurveValue * _movement.y;
+            Vector3 forward = _pointsArray[currentIndex].transform.forward * combinedCurveValue * _movement.z;
+            Vector3 movement = up + right + forward;
+
+            //Apply movement
+            if (currentIndex == 0 || currentIndex == _pointsArray.Length - 1) { // don't move start and end point
+                movement = Vector3.zero;
+            } else {
+                _pointsArray[currentIndex].transform.position = _startPointLocation[currentIndex] + movement;
+            }
+        }
+    }
 
 }
--- Assets/Script/MidiInputControll.cs
+++ Assets/Script/MidiInputControll.cs
@@ -23,40 +23,46 @@ public class MidiInputControll : MonoBehaviour {
         var midiDevice = (MidiDevice)sender;
         //Debug.Log($"Received MIDI event {e.Event} from {midiDevice.Name}");
         Debug.Log(e.Event);
-
+        
         // if (e.Event is NoteOnEvent noteOn) {
         //     Debug.Log($"Note {noteOn.NoteNumber} pressed with velocity {noteOn.Velocity}");
         // }
 
         if (e.Event is ControlChangeEvent controlChange) {
+
+            if (e.Event.EventType == MidiEventType.ControlChange){
+
+            }
             Debug.Log($"Control {controlChange.ControlNumber} changed to {controlChange.ControlValue}");
-            // Map 0-127 to -10 to 10
-            float mappedValue = (controlChange.ControlValue / 127f) * 20f - 10f;   
-            if(controlChange.ControlNumber == 1){
-                _csCreateLine._amplitudeA = mappedValue;
-            }
-            if(controlChange.ControlNumber == 2){
-                _csCreateLine._amplitudeB = mappedValue;
-            }
-            if(controlChange.ControlNumber == 3){
-                _csCreateLine._amplitudeC = mappedValue;
-            }
-            if(controlChange.ControlNumber == 4){
-                _csCreateLine._amplitudeD = mappedValue;
-            }
-            if(controlChange.ControlNumber == 5){
-                _csCreateLine._amplitudeE = mappedValue;
-            }
-            if(controlChange.ControlNumber == 6){
-                _csCreateLine._amplitudeF = mappedValue;
-            }
-            if(controlChange.ControlNumber == 10){
-                _csCreateLine._frequency = controlChange.ControlValue/1;
-            }
+
             
-            if(controlChange.ControlNumber == 11){
-                _csCreateLine._horizontalMovement = 2*controlChange.ControlValue;
-            }
+            // // Map 0-127 to -10 to 10
+            // float mappedValue = (controlChange.ControlValue / 127f) * 20f - 10f;   
+            // if(controlChange.ControlNumber == 1){
+            //     _csCreateLine._amplitudeA = mappedValue;
+            // }
+            // if(controlChange.ControlNumber == 2){
+            //     _csCreateLine._amplitudeB = mappedValue;
+            // }
+            // if(controlChange.ControlNumber == 3){
+            //     _csCreateLine._amplitudeC = mappedValue;
+            // }
+            // if(controlChange.ControlNumber == 4){
+            //     _csCreateLine._amplitudeD = mappedValue;
+            // }
+            // if(controlChange.ControlNumber == 5){
+            //     _csCreateLine._amplitudeE = mappedValue;
+            // }
+            // if(controlChange.ControlNumber == 6){
+            //     _csCreateLine._amplitudeF = mappedValue;
+            // }
+            
+            // if(controlChange.ControlNumber == 10){
+            //     _csCreateLine._frequency = controlChange.ControlValue;
+            // }
+            // if(controlChange.ControlNumber == 11){
+            //     _csCreateLine._horizontalMovement = 2*controlChange.ControlValue;
+            // }
             
         }
         
