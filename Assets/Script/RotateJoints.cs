f54b1159eea44ac5d1b2a59028cd38a24670ef8a
LisaBach <91595869+julielionheart@users.noreply.github.com>
2025-02-18 15:31:52 +0100

    Script Rotation

---
+++ Assets/Script/RotateJoints.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateJoints : MonoBehaviour
+{
+    [SerializeField] private CreateLine _csCreateLine;
+    [SerializeField] private GameObject _positionReference;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
