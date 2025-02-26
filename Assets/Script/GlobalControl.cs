1475c9a497bfebe9cf9dccb5cb24043843483982
JanThuering <jp.thuering@bluewin.ch>
2025-02-26 17:58:55 +0100

    Midi to Unity
    - wave switch fehlt

--- Assets/Script/GlobalControl.cs
+++ Assets/Script/GlobalControl.cs
@@ -1,56 +1,137 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class GlobalControl : MonoBehaviour
 {
-    [Header("References to parameters")]
 
-    //ANIMATIONS
+    //Singleton Instance
+    public static GlobalControl Instance {get; private set;}
+
+    [Header("EXTERNAL REFERENCES")]
+    //ANIMATIONS-PARAMETERS
     [SerializeField] private PuppetAnimation puppetAnimation;
+    [SerializeField] private CreateLine createLine;
     private float animationMultiplier; //effects the amount of movement of the puppet
     private float scaleMultiplier; //effects the amount of scaling of the controlpoints
 
-    //WAVE
-    [SerializeField] private CreateLine createLine;
-    private float globalAmplitude;
-    private float globalFrequency;
-    private float globalSpeed;
+    [Header("LIMITS")]
+    [Header("Amplitude")]
+    [SerializeField] private float minAmplitude = 0;
+    [SerializeField] private float maxAmplitude = 5;
+    [Header("Frequency")]
+    [SerializeField] private float minFrequency = 1;
+    [SerializeField] private float maxFrequency = 10;
+    [Header("Speed")]
+    [SerializeField] private float minSpeed = 1;
+    [SerializeField] private float maxSpeed = 5;
+
+
+
+    //WAVE-PARAMETERS
+    [Header("GLOBAL WAVE")]
+    [SerializeField] private float globalAmplitude;
+    [SerializeField] private float globalFrequency;
+    [SerializeField] private float globalSpeed;
+    [SerializeField] private int [] waveType =  {0, 0, 0, 0}; 
+
+    public int [] WaveType
+    {
+        get => waveType;
+        set => waveType = value;
+    }
+
+    public float GlobalAmplitude
+    {
+        get => globalAmplitude;
+        set => globalAmplitude = Mathf.Clamp(value, minAmplitude, maxAmplitude);
+    }
+    public float GlobalFrequency
+    {
+        get => globalFrequency;
+        set => globalFrequency = Mathf.Clamp(value, minFrequency, maxFrequency);
+    }
+    public float GlobalSpeed
+    {
+        get => globalSpeed;
+        set => globalSpeed = Mathf.Clamp(value, minSpeed, maxSpeed);
+    }
+
+
     //wave - curveA
-    private float amplitudeA;
-    private float speedA;
+    [Header ("Curve A")]
+    [SerializeField] private float amplitudeA;
+    [SerializeField] private float speedA;
+    public float AmplitudeA
+    {
+        get => amplitudeA;
+        set => amplitudeA = Mathf.Clamp(value, minAmplitude, maxAmplitude);
+    }
+    public float SpeedA
+    {
+        get => speedA;
+        set => speedA = Mathf.Clamp(value, minSpeed, maxSpeed);
+    }
     //wave - curveB
-    private float amplitudeB;
-    private float speedB;
+    [Header ("Curve B")]
+    [SerializeField] private float amplitudeB;
+    [SerializeField] private float speedB;
+    public float AmplitudeB
+    {
+        get => amplitudeB;
+        set => amplitudeB = Mathf.Clamp(value, minAmplitude, maxAmplitude);
+    }
+    public float SpeedB
+    {
+        get => speedB;
+        set => speedB = Mathf.Clamp(value, minSpeed, maxSpeed);
+    }
     //wave - curveC
-    private float amplitudeC;
-    private float speedC;
+    [Header ("Curve C")]
+    [SerializeField] private float amplitudeC;
+    [SerializeField] private float speedC;
+    public float AmplitudeC
+    {
+        get => amplitudeC;
+        set => amplitudeC = Mathf.Clamp(value, minAmplitude, maxAmplitude);
+    }
+    public float SpeedC
+    {
+        get => speedC;
+        set => speedC = Mathf.Clamp(value, minSpeed, maxSpeed);
+    }
     //wave - curveD
-    private float amplitudeD;
-    private float speedD;
+    [Header ("Curve D")]
+    [SerializeField] private float amplitudeD;
+    [SerializeField] private float speedD;
+    public float AmplitudeD
+    {
+        get => amplitudeD;
+        set => amplitudeD = Mathf.Clamp(value, minAmplitude, maxAmplitude);
+    }
+    public float SpeedD
+    {
+        get => speedD;
+        set => speedD = Mathf.Clamp(value, minSpeed, maxSpeed);
+    }
+
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this){
+            Destroy(this);
+        }else{
+            Instance = this;
+        }
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
-        //puppet
-        animationMultiplier = puppetAnimation.MovementMultiplier;
-        scaleMultiplier = puppetAnimation.ScaleMultiplier;
-        //wave - global values
-        globalAmplitude = createLine.GlobalAmplitude;
-        globalFrequency = createLine.GlobalFrequency;
-        globalSpeed = createLine.GlobalSpeed;
-        //wave - curveA
-        amplitudeA = createLine.AmplitudeA;
-        speedA = createLine.SpeedA;
-        //wave - curveB
-        amplitudeB = createLine.AmplitudeB;
-        speedB = createLine.SpeedB;
-        //wave - curveC
-        amplitudeC = createLine.AmplitudeC;
-        speedC = createLine.SpeedC;
-        //wave - curveD
-        amplitudeD = createLine.AmplitudeD;
-        speedD = createLine.SpeedD;
+        SetStartValues();
     }
 
     // Update is called once per frame
@@ -58,4 +139,77 @@ public class GlobalControl : MonoBehaviour
     {
 
     }
+
+    private void SetStartValues(){
+        GlobalAmplitude = 1;
+        GlobalFrequency = 1;
+        GlobalSpeed = 1;
+
+        AmplitudeA = 1;
+        SpeedA = 1;
+
+        AmplitudeB = 1;
+        SpeedB = 1;
+
+        AmplitudeC = 1;
+        SpeedC = 1;
+
+        AmplitudeD = 1;
+        SpeedD = 1;
+    }
+
+    public float GetMaxAmplitude(){
+        return maxAmplitude;
+    }
+
+    public void MidiGlobalWave(int controlNumber, float controlValue, float valueAmmount){
+        /*VALUE EXPLANATION
+            controlNumber -> slider (for which slider the curve is)
+            controlValue -> waveType (amplitude of the curve)
+            valueAmmount -> max value of the slider (how to distribute the waveTypes on the values)
+        */
+        switch(controlNumber){
+            case 7: GlobalAmplitude = controlValue / valueAmmount * (maxAmplitude-minAmplitude) + minAmplitude; break;
+            case 8: GlobalFrequency = controlValue / valueAmmount * (maxFrequency-minFrequency) + minFrequency; break;
+            case 9: GlobalSpeed = controlValue / valueAmmount * (maxSpeed-minSpeed) + minSpeed; break;
+        }
+
+    }
+
+    public void MidiAmplitudeWave(int controlNumber, float controlValue, float maxValue){
+        /*VALUE EXPLANATION
+            controlNumber -> segment (which curve is selected)
+            controlValue -> waveType (amplitude or speed of the curve)
+            maxValue -> max value of the slider (how to distribute the waveTypes on the values)
+        */
+        float increments = controlValue / maxValue * (maxAmplitude-minAmplitude) + minAmplitude;    //calculate the value of the slider / knob
+
+        switch(controlNumber){
+            case 1: AmplitudeA = increments; break;
+            case 2: AmplitudeB = increments; break;
+            case 3: AmplitudeC = increments; break;
+            case 4: AmplitudeD = increments; break;
+        }
+    }
+
+    public void MidiSpeedWave(int controlNumber, float controlValue, float valueAmmount){
+        /*VALUE EXPLANATION
+            controlNumber -> slider (for which slider the curve is)
+            controlValue -> waveType (amplitude of the curve)
+            valueAmmount -> max value of the slider (how to distribute the waveTypes on the values)
+        */
+
+        float increments = controlValue / valueAmmount * (maxSpeed-minSpeed) + minSpeed;    //calculate the value of the slider / knob
+
+        switch(controlNumber){
+            case 10: speedA = increments; break;
+            case 11: speedB = increments; break;
+            case 12: speedC = increments; break;
+            case 13: speedD = increments; break;
+        }
+
+    }
+
+
+
 }
--- Assets/Script/MidiInputControll.cs
+++ Assets/Script/MidiInputControll.cs
@@ -5,10 +5,16 @@ using Melanchall.DryWetMidi.Core;
 public class MidiInputControll : MonoBehaviour {
     [SerializeField] InputDevice input;
     CreateLine createLineScript;
+    GlobalControl globalControlScript;
 
     private void Start()
     {
+        FillAllParameters();
+    }
+
+    private void FillAllParameters(){
         createLineScript = CreateLine.Instance;
+        globalControlScript = GlobalControl.Instance;
     }
 
     void OnEnable() {
@@ -27,21 +33,30 @@ public class MidiInputControll : MonoBehaviour {
 
     void OnEventReceived(object sender, MidiEventReceivedEventArgs e) {
         var midiDevice = (MidiDevice)sender;
-        //Debug.Log($"Received MIDI event {e.Event} from {midiDevice.Name}");
-        //Debug.Log(e.Event);
-        
-        // if (e.Event is NoteOnEvent noteOn) {
-        //     Debug.Log($"Note {noteOn.NoteNumber} pressed with velocity {noteOn.Velocity}");
-        // }
 
         if (e.Event is ControlChangeEvent controlChange) {
-
-            //Debug.Log($"ControlNumber: {controlChange.ControlNumber} ControlValue: {controlChange.ControlValue}");
-
-            if(controlChange.ControlNumber >= 1 && controlChange.ControlNumber <= 4){
-                createLineScript.MidiAmplitudeWave(controlChange.ControlNumber, controlChange.ControlValue, 127f);
+            //GLOBAL - Control the wave in GlobalControl
+            if(controlChange.ControlNumber >= 7 && controlChange.ControlNumber <= 9){
+                globalControlScript.MidiGlobalWave(controlChange.ControlNumber, controlChange.ControlValue, 127f);
             }
 
+            //AMPLITUDE - Control amplitudes of the individual Waves 
+            if(controlChange.ControlNumber >= 1 && controlChange.ControlNumber <= 4){
+                globalControlScript.MidiAmplitudeWave(controlChange.ControlNumber, controlChange.ControlValue, 127f);
+            }
+
+            //SPEED - Control speed of the individual Waves
+            if(controlChange.ControlNumber >= 10 && controlChange.ControlNumber <= 13){
+                globalControlScript.MidiSpeedWave(controlChange.ControlNumber, controlChange.ControlValue, 127f);
+            }
+
+            // if(controlChange.ControlNumber >= 1 && controlChange.ControlNumber <= 4){
+            //     createLineScript.MidiAmplitudeWave(controlChange.ControlNumber, controlChange.ControlValue, 127f);
+            // }
+
+            // if(controlChange.ControlNumber >= 10 && controlChange.ControlNumber <= 13){
+            //     createLineScript.MidiSpeedWave(controlChange.ControlNumber, controlChange.ControlValue, 127f);
+            // }
 
             // //Control the curve in CreateLineScript
             // if (controlChange.ControlNumber < 20){
@@ -50,9 +65,6 @@ public class MidiInputControll : MonoBehaviour {
             // if(controlChange.ControlNumber > 20){
             //     createLineScript.MidiDefineWaveType(controlChange.ControlNumber, controlChange.ControlValue, 127f);
             // }
-            
-
-            
         }
         
         
