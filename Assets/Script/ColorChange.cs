1ccf73639360468675023d8e19bf072faf6c25fb
LisaBach <91595869+julielionheart@users.noreply.github.com>
2025-03-03 11:49:32 +0100

    color change implemented

--- Assets/Script/ColorChange.cs
+++ Assets/Script/ColorChange.cs
@@ -1,25 +1,108 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ColorChange : MonoBehaviour
 {
+    [Range(0, 127)]
+    public float CurrentColor = 127/2;
+    private int amountOfColorEffects = 3;
+    private bool isColorChange1 = false;
+    private bool isColorChange2 = false;
+
     [Header("Materials")]
+    [SerializeField] private Material[] currentMaterials;
     [SerializeField] private Material[] baseMaterials;
-    [Header("Color Change 1 Materials")]
     [SerializeField] private Material[] colorChange1Materials;
-    [Header("Color Change 2 Materials")]
     [SerializeField] private Material[] colorChange2Materials;
+
+    //Checks
+    private bool isbaseColorApplied = false;
+    private bool isColorChange1Applied = false;
+    private bool isColorChange2Applied = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        if (isColorChange1 == false && isColorChange2 == false && isbaseColorApplied == false) BaseColor();
     }
 
     // Update is called once per frame
     void Update()
     {
+        ColorApplier();
+        if (isColorChange1 == false && isColorChange2 == false && isbaseColorApplied == false) BaseColor();
+        if (isColorChange1 && isColorChange1Applied == false) ColorEffectOne();
+        if (isColorChange2 && isColorChange2Applied == false) ColorEffectTwo();
+    }
 
+    private void ColorApplier()
+    {
+        float effectOneRange = 127 / amountOfColorEffects; 
+        float baseRange = effectOneRange * 2;
+        float effectTwoRange = effectOneRange * 3;
+
+        if (CurrentColor <= effectOneRange)
+        {
+            isColorChange1 = true;
+            isColorChange2 = false;
+        }
+        else if (CurrentColor <= baseRange)
+        {
+            isColorChange1 = false;
+            isColorChange2 = false;  
+        }
+        else if (CurrentColor <= effectTwoRange)
+        {
+            isColorChange1 = false;
+            isColorChange2 = true;
+        }
+    }
+
+    private void BaseColor()
+    {
+        isbaseColorApplied = true;
+        isColorChange1Applied = false;
+        isColorChange2Applied = false;
+
+        if(isColorChange1 || isColorChange2) 
+        {
+            isColorChange1 = false;
+            isColorChange2 = false;
+        }
+        
+        ApplyColor(baseMaterials);
+    }
+
+    private void ColorEffectOne()
+    {
+        isbaseColorApplied = false;
+        isColorChange1Applied = true;
+        isColorChange2Applied = false;
+
+        if(isColorChange2) isColorChange2 = false;
+
+        ApplyColor(colorChange1Materials);
+    }
+    private void ColorEffectTwo()
+    {
+        isbaseColorApplied = false;
+        isColorChange1Applied = false;
+        isColorChange2Applied = true;
+
+        if(isColorChange1) isColorChange1 = false;
+
+        ApplyColor(colorChange2Materials);
+    }
+
+    private void ApplyColor(Material[] colorUpdateMaterial)
+    {
+        for (int i = 0; i < currentMaterials.Length; i++)
+        {
+            currentMaterials[i].color = colorUpdateMaterial[i].color;
+        }
     }
 }
