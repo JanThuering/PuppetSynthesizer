2f7b1bf8626c0451ff175f8c09f145129baf6ecd
LisaBach <91595869+julielionheart@users.noreply.github.com>
2025-03-03 10:31:49 +0100

    Material rename, Material references on script gamemanager prefab applied

--- Assets/Script/ColorChange.cs
+++ Assets/Script/ColorChange.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class ColorChange : MonoBehaviour
 {
+    [Header("Background Material")]
+    [SerializeField] private Material bgMaterial;
+    [Header("Puppet Materials")]
+    [SerializeField] private Material puppetBaseMaterial;
+    [SerializeField] private Material puppetAccentMaterial;
+    [Header("Control Points Materials")]
+    [SerializeField] private Material controlPointMaterial;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
