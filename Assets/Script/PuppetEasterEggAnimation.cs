7e04487e7f40d7ebe0b335994f255375f3a8ccdb
LisaBach <91595869+julielionheart@users.noreply.github.com>
2025-03-06 11:46:37 +0100

    Update PuppetEasterEggAnimation.cs

--- Assets/Script/PuppetEasterEggAnimation.cs
+++ Assets/Script/PuppetEasterEggAnimation.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
+using System;
+using UnityEngine.Animations.Rigging;
+using DG.Tweening;
 
 public class PuppetEasterEggAnimation : MonoBehaviour
 {
+    private Animator animator;
+    [SerializeField] private GameObject[] animationRigs;
+
     // Start is called before the first frame update
+
+    void OnEnable()
+    {
+        // GlobalControl.OnPirouette += Pirouette;
+    }
+
+    void OnDisable()
+    {
+        // GlobalControl.OnPirouette -= Pirouette;
+    }
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void PirouetteStart()
+    {
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Pirouette"))
+        {
+            for (int i = 0; i < animationRigs.Length; i++)
+            {
+                animationRigs[i].GetComponent<TwistChainConstraint>().weight = 0;
+            }
+            animator.SetBool("isPirouette", true);
+            gameObject.transform.DORotate(Vector3.one * 360, 4).
+            OnComplete(() => PirouetteStop());
+
+        }
+    }
+
+    private void PirouetteStop()
+    {
+        for (int i = 0; i < animationRigs.Length; i++)
+        {
+            animationRigs[i].GetComponent<TwistChainConstraint>().weight = 1;
+        }
+        animator.SetBool("isPirouette", false);
     }
 }
